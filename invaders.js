103307100c3f1fca4c95cfd1419a13a5a17a2a92
Dag Holmberg <holmberd@gmail.com>
2016-09-07 16:58:32 -0700

    fix: invaders switch velocity vector at edge

--- invaders.js
+++ invaders.js
@@ -1,18 +1,21 @@
 var invaders = (function() {
 	var invaders = {};
-	var NUM_OF_INVADERS = 50;
+	var NUM_OF_INVADERS = 3;
 	var HORIZONTAL = 0;
 	var VERTICAL = 1;
 	var HORIZONTAL_VELOCITY = 1;
 	var VERTICAL_VELOCITY = 1;
 	var MAX_HORIZONTAL_VELOCITY = 5;
 	var MAX_VERTICAL_VELOCITY = 1;
-	invaders.velocity = [HORIZONTAL_VELOCITY, VERTICAL_VELOCITY];
+	invaders.velocity = new Vector(HORIZONTAL_VELOCITY, VERTICAL_VELOCITY);
 	invaders.list = new List();
 
-	invaders.init = function() {
+	var map = [new Vector(7,5),new Vector(8,5), new Vector(9,5)];
+	var sprite = '#';
+
+	invaders.init = function(map) {
 		for (var n = 0; n < NUM_OF_INVADERS; n++) {
-			var invader = new Entity();
+			var invader = new Entity(map[n], sprite);
 			invader.addComponent(new Components.Group('01'));
 			invader.addComponent(new Components.Health(1));
 			invader.addComponent(new Components.Collision());
@@ -28,29 +31,30 @@ var invaders = (function() {
 
 		}
 
-		if (atEdge(grid, this.list, this.velocity)) { // invaders reached outer grid edge
-			this.velocity[HORIZONTAL] *= -1; // negative => postive & postive => negative
-			if (this.velocity[HORIZONTAL] > 0) { // increase invaders velocity
-				this.velocity[HORIZONTAL]++;
+		if (atEdge.call(this, grid, this.velocity)) { // invaders reached outer grid edge
+			console.log('DEBUG: at edge');
+			this.velocity.x *= -1; // negative => postive & postive => negative
+			if (this.velocity.x > 0) { // increase invaders velocity
+				this.velocity.x++;
 			} else {
-				this.velocity[HORIZONTAL]--;
+				this.velocity.x--;
 			}
-			updatePosition(this.velocity[HORIZONTAL], this.velocity[VERTICAL]); // move invaders down one step
+			updatePosition.call(this, this.velocity.x, this.velocity.y); // move invaders down one step
 		} else {
-			updatePosition(this.velocity[HORIZONTAL]); // move invaders one step
+			updatePosition.call(this, this.velocity.x); // move invaders one step
 		}
 
 		function updatePosition(horizontal, vertical) {
 			var vector = new Vector(horizontal, vertical || 0);
 			for (var invader = this.head; invader; invader = invader.next) { // FIX: replace for a list.map() / list.forEach() method
-				invader.vector.plus(vector);
+				invader.element.vector = invader.element.vector.plus(vector);
 			}
 		}
 
-		function atEdge(grid, invaders, velocity) {
+		function atEdge(grid, velocity) {
 			var vector = null;
 			for (var invader = this.head; invader; invader = invader.next) {
-				vector = new Vector(invader.vector.x + velocity, invader.vector.y);
+				vector = new Vector(invader.element.vector.x + velocity.x, invader.element.vector.y);
 				if (!grid.isInside(vector)) {
 					return true;
 				}

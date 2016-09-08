04818d33be5de7834dff226a487f45a9f8f02ac7
Dag Holmberg <holmberd@gmail.com>
2016-09-07 17:02:03 -0700

    updated world.init to handle previous changes

--- world.js
+++ world.js
@@ -1,9 +1,12 @@
-
-function World(map, entities, canvas) {
+function World(map, entities) {
 	var grid = new Grid(map.grid.height, map.grid.width);
 	this.grid = grid;
 	this.entities = entities;
-	this.init = function() {
+	this.init = function(map, canvas) {
+		entities.player.init(map);
+		entities.invaders.init(map);
+		//entities.bullets.init();
+		systems.render.init(canvas, this.grid);
 		systems.input.init(canvas);
 	};
 

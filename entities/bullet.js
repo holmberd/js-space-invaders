39f636d9612764d6c0f6a23503934734f388da2b
Dag Holmberg <holmberd@gmail.com>
2016-09-15 18:51:33 -0700

    rewrote: bullets are now created and delegated from the bullet module.

--- entities/bullet.js
+++ entities/bullet.js
@@ -1,4 +1,4 @@
-// /js/entities/bullet.js
+// /js/entities/Bullet.js
 
 var Point = require('../utils/utils.math.js');
 var Entity = require('./entity.js');
@@ -6,46 +6,82 @@ var Entity = require('./entity.js');
 /** Bullet Module
  * Main bullet entity module.
  */
-function Bullet(scope, x, y) {
-    var SPRITE_HEIGHT = 3,
-        SPRITE_WIDTH = 3,
-        SPRITE_IMAGE = null,
-        BULLET_SPEED = 3;
-        BULLET_HEALTH = 1;
 
-    var point = new Point(x, y);
+function createBullets(scope) {
+    var SPRITE_HEIGHT = 8,
+    SPRITE_WIDTH = 3,
+    SPRITE_IMAGE = null,
+    BULLET_SPEED = 6,
+    BULLET_HEALTH = 1,
+    BULLET_GROUP_NAME = 'bullet',
+    BULLET_POINT = null,
+    NUM_OF_BULLETS = 200;
+
     var sprite = {
         height: SPRITE_HEIGHT,
         width: SPRITE_WIDTH,
         image: SPRITE_IMAGE
     };
 
-    var bullet = new Entity(point, BULLET_SPEED, BULLET_HEALTH, sprite);
-    // set bullet property to 'invader' owned
-    bullet.pc = true;
+    // instantiate the delegate object (it is basicly a pointer to a prototype chain of methods)
+    var delegateObj = new Bullet();
 
-    // Draw the bullet on the canvas
-    bullet.render = function bulletRender() {
-        scope.context.fillStyle = '#40d870';
-        scope.context.fillRect(
-            bullet.state.position.x,
-            bullet.state.position.y,
-            sprite.width, sprite.height
-        );
-    };
-
-    // Fired via the global update method.
-    // Mutates state as needed for proper rendering next state
-    bullet.update = function bulletUpdate() {
-        var point = new Point(0, bullet.state.position.y);
-        if (bullet.inBoundary(scope, point) && !bullet.state.killed) {
-            bullet.state.position.y -= bullet.state.moveSpeed;
-        } else {
-            delete scope.state.entities[bullet.id];
-        }
-    };
-
-    return bullet;
+    // Instantiate bullets to store as inactive entities, 
+    // this prevent us from instantiating every time a bullet is fired
+    for (var n = 0, bullet = {}; n < NUM_OF_BULLETS; n++) {
+        bullet = new Entity(BULLET_GROUP_NAME, BULLET_POINT, BULLET_SPEED, BULLET_HEALTH, sprite);
+        bullet.delegate = delegateObj;
+        // 'pc' property default to false as player bullet, otherwise invader(pc)
+        bullet.pc = false;
+        scope.state.inactiveEntities.bullets.push(bullet);
+    }
 }
 
-module.exports = PlayerBullet;
+function Bullet() {}
+
+Bullet.prototype.render = function bulletRender(scope) { // FIX: DRY = DONT REPEAT YOURSELF, seperate delegate class for bullets, invaders and player.
+    scope.context.fillStyle = '#40d870';
+    scope.context.fillRect(
+        this.state.position.x,
+        this.state.position.y,
+        this.sprite.width, 
+        this.sprite.height
+    );
+};
+
+Bullet.prototype.update = function bulletUpdate(scope) {
+        var point = new Point(0, this.state.position.y);
+        if (this.inBoundary(scope)) {
+            this.state.position.y -= this.state.moveSpeed;
+        } else {
+            this.kill(); 
+            return this;
+            //delete scope.state.entities[bullet.id]; // FIX: decouple from game state by setting flag to `killed` and handle in game update ? Removes need for including `scope` i.e. state in function.
+        }
+};
+
+Bullet.prototype.collision = function(entity) {
+    if (this.hasCollidedWith(entity)) {
+        if (!this.pc && entity.group === 'player') {
+            return this;
+        } else {
+        console.log('Event: Bullet has collided with something');
+        // Set bullets state to killed
+        this.kill();
+        // call collision on the entity collided with
+        entity.collision(this);
+        }
+    }
+    /*
+    if (this.hasCollidedWith(entity)) {
+        console.log('Event: block collided with bullet');
+        // Set bullets state to killed
+        this.kill();
+        // call collision on the entity collided with
+        entity.collision(this);
+        //delete scope.state.entities[entity.id];
+    } */
+    return this;
+};
+
+module.exports = createBullets;

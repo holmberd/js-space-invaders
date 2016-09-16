4c6d89ca64362b1540c600d22b7332006c3ca170
Dag Holmberg <holmberd@gmail.com>
2016-09-16 11:06:02 -0700

    added: deadCleanUp func to clean up entities that are 'killed' flagged

--- core/game.collision.js
+++ core/game.collision.js
@@ -11,20 +11,35 @@
     return function collision(tFrame) {
         var state = scope.state || {};
 
-        // If there are entities, iterate through them and call their 'collision' methods
+        // Cleans up entities that are `killed` flagged
+        function deadCleanUp(entities) {
+            for (var entity in entities) {
+                if (entities[entity].state.killed) {
+                    delete state.entities[entity];
+                } else {
+                    continue;
+                }
+            }
+            return;
+        }
+
+        // If there are entities, iterate through them and call their `collision` methods
         if (state.hasOwnProperty('entities')) {
             var entities = state.entities;
             for (var entity in entities) {
-                // For each 'bullet' entity check for collision with other active entities
-            	if (entities[entity].group === 'bullet') {
-            		for (var entityOther in entities) {
-            			if (entities[entityOther].collides && entityOther !== entity) {
-            				// Fire off each active entities 'collision' method
-            				entities[entity].collision(entities[entityOther]);            				
+                // If `killed` flag set, skip entity collision check
+                if (entities[entity].state.killed) {
+                    continue;
+                } else if (entities[entity].group === 'bullet') { // For each `bullet` entity check for collision with other active entities
+            		for (var entityOther in entities) { 
+            			if (entities[entityOther].collides && entityOther !== entity && !entities[entityOther].state.killed) {
+            				// Fire off each active entities `collision` method
+                            entities[entity].collision(entities[entityOther]);
             			} else continue;
             		}
             	}
             }
+            deadCleanUp(entities);
         }
 
         return state;

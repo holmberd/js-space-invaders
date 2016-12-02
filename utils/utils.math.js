d19c7dcf8c38fbe3f359d4e3a97b27b7a217fc87
Dag Holmberg <holmberd@gmail.com>
2016-12-02 10:13:10 -0800

    moved getRandomInt from invaders module to utils.math

--- utils/utils.math.js
+++ utils/utils.math.js
@@ -1,10 +1,11 @@
 // /utils/utils.math.js
 
 /** Math Module
- * Contains the main Point class that has helper
- * methods for doing calculations on positioning in a grid.
+ * Contains helper methods for doing calculations.
  */
 
+// Point object constructor
+// Contains methods for vector calculations.
 function Point(x, y) {
 	this.x = x || 0;
 	this.y = y || 0;
@@ -12,21 +13,32 @@ function Point(x, y) {
 
 Point.prototype.constructor = Point;
 
+// If y is omitted, both x and y will be set to x.
 Point.prototype.set = function (x, y){
-    this.x = x || 0;
-    this.y = y || ( (y !== 0) ? this.x : 0 ); // If y is omitted, both x and y will be set to x.
+  this.x = x || 0;
+  this.y = y || ( (y !== 0) ? this.x : 0 );
 };
 
 Point.prototype.clone = function () {
-    return new Point(this.x, this.y);
+  return new Point(this.x, this.y);
 };
 
 Point.prototype.copy = function (p) {
-    this.set(p.x, p.y);
+  this.set(p.x, p.y);
 };
 
 Point.prototype.equals = function (p) {
-    return (p.x === this.x) && (p.y === this.y);
+  return (p.x === this.x) && (p.y === this.y);
 };
 
-module.exports = Point;
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+module.exports = {
+  Point: Point,
+  getRandomInt: getRandomInt
+};
